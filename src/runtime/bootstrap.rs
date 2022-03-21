503bc13c6acb00ec9a2fc827d8d6994ae2e77710
nullishamy <amycodes@null.net>
2022-03-21 18:17:17 +0000

    feat(vm): initialise VM
    this initialises the structures used by the VM, including the bootstrap
    class loader

---
+++ src/runtime/bootstrap.rs
@@ -0,0 +1,16 @@
+use crate::parse::classfile::ClassFile;
+use crate::runtime::loaded_class::LoadedClass;
+
+struct BootstrapClassLoader<'a> {
+    classes: Vec<LoadedClass<'a>>,
+}
+
+impl<'a> BootstrapClassLoader<'a> {
+    pub fn new() -> Self {
+        Self { classes: vec![] }
+    }
+
+    pub fn load_class(&self, class_file: ClassFile) -> LoadedClass {
+        LoadedClass::new(class_file)
+    }
+}
---
+++ src/runtime/mod.rs
@@ -0,0 +1,3 @@
+mod bootstrap;
+mod loaded_class;
+mod vm;
