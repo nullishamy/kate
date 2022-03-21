a2728b30fd5676cb72b4a1d370f238529cbdd864
nullishamy <amycodes@null.net>
2022-03-21 21:39:01 +0000

    feat(types): add primitive types
    this adds a value-less enum for primitives, and refactors the previous
    enum to accurately represent it.

--- src/types/primitive.rs
+++ src/types/primitive.rs
@@ -5,7 +5,29 @@ pub type Byte = i8;
 pub type Short = i16;
 pub type Int = i32;
 pub type Long = i64;
-pub type Char = i16;
+pub type Char = char;
 pub type Float = f32;
 pub type Double = f64;
 pub type ReturnAddress = JVMPointer;
+
+pub enum PrimitiveWithValue {
+    Boolean(Boolean),
+    Byte(Byte),
+    Short(Short),
+    Int(Int),
+    Long(Long),
+    Char(Char),
+    Float(Float),
+    Double(Double),
+}
+#[derive(Debug)]
+pub enum PrimitiveType {
+    Boolean,
+    Byte,
+    Short,
+    Int,
+    Long,
+    Char,
+    Float,
+    Double,
+}
