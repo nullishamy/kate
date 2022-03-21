ef04187883b134e6e8c5a756d5cc1c8a2f5dbad5
nullishamy <amycodes@null.net>
2022-03-21 13:16:59 +0000

    chore: refactor primitive types
    this refactors the primitive types out to a seperate module

---
+++ src/types/primitive.rs
@@ -0,0 +1,11 @@
+use crate::types::JVMPointer;
+
+pub type Boolean = bool;
+pub type Byte = i8;
+pub type Short = i16;
+pub type Int = i32;
+pub type Long = i64;
+pub type Char = i16;
+pub type Float = f32;
+pub type Double = f64;
+pub type ReturnAddress = JVMPointer;
