abafd28ce40a752ed51aed50e7973078908a7e1e
nullishamy <amycodes@null.net>
2022-03-29 00:46:32 +0100

    feat(cli): update main
    this updates the main method to comply with recent VM changes.
    main now uses the VM type instead of manually constructing a
    parser and loading the class.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::borrow::Borrow;
 use std::env;
+use std::rc::Rc;
 
 use crate::classfile::parse::ClassFileParser;
+use crate::runtime::classload::loader::{ClassLoader, MutableClassLoader};
 use crate::runtime::classload::system::SystemClassLoader;
+use crate::runtime::context::Context;
+use crate::runtime::threading::thread::VMThread;
+use crate::runtime::vm::VM;
+use crate::structs::bitflag::MethodAccessFlag;
+use crate::structs::descriptor::{DescriptorType, ReferenceType};
 use crate::structs::loaded::classfile::LoadedClassFile;
 use anyhow::{anyhow, Result};
 
@@ -21,12 +28,47 @@ fn main() -> Result<()> {
 }
 
 fn start(class_path: &str) -> Result<()> {
-    let mut parser = ClassFileParser::from_path(class_path.to_string())?;
-    let class_file = parser.parse()?;
-    let class_file = LoadedClassFile::from_raw(class_file)?;
+    let mut vm = VM::new();
+
+    let loader = vm.system_classloader();
+    let main_class = loader.find_class(class_path)?;
+    let (loader, main_class) = loader.define_class(&main_class)?;
+
+    let main_method = main_class.methods.find(|m| {
+        m.name.as_str == "main"
+            && m.access_flags.has(MethodAccessFlag::STATIC)
+            && m.descriptor.return_type == DescriptorType::Void
+            && m.descriptor.parameters.len() == 1
+            && m.descriptor
+                .parameters
+                .get(0)
+                .unwrap()
+                .as_array()
+                .filter(|p| {
+                    p._type
+                        .as_reference()
+                        .filter(|a| a.internal_name == "java/lang/String")
+                        .is_some()
+                })
+                .is_some()
+    });
+
+    if main_method.is_none() {
+        return Err(anyhow!("main method not found"));
+    }
+
+    let main_method = main_method.unwrap();
+
+    vm.interpret(
+        main_method,
+        &mut Context {
+            class: Rc::clone(&main_class),
+            thread: VMThread::new(),
+        },
+    )?;
+
     Ok(())
 }
-
 fn quit(msg: &str) -> ! {
     eprintln!("[ERROR] {}", msg);
     std::process::exit(1);
