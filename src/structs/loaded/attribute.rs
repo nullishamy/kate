c05498071c239018268bb414322f0489d3acb094
nullishamy <amycodes@null.net>
2022-03-29 00:24:02 +0100

    feat(parse): add new attribute parsing system
    this lays the groundwork for the new attribute system.
    this system will implement default attributes as defined
    in the JVM spec.

--- src/structs/loaded/attribute.rs
+++ src/structs/loaded/attribute.rs
@@ -1,10 +1,21 @@
-#[derive(Clone)]
-pub struct AttributeEntry {
-    pub name: String,
-    pub data: Vec<u8>,
-}
+use crate::structs::loaded::constant_pool::Utf8Data;
+use crate::structs::loaded::default_attributes::{AttributeEntry, CustomData};
+use anyhow::{anyhow, Result};
+use std::rc::Rc;
 
 #[derive(Clone)]
 pub struct Attributes {
     pub entries: Vec<AttributeEntry>,
+}
+
+impl Attributes {
+    pub fn get(&self, key: &str) -> Option<&AttributeEntry> {
+        // deref one layer, the vec lookup
+        self.entries
+            .iter()
+            .filter(|a| a.name().as_str == key)
+            .collect::<Vec<&AttributeEntry>>()
+            .first()
+            .map(|r| *r)
+    }
 }
