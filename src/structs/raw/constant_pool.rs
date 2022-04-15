26048a2dc0726e2a16264c5430d2021536f512e7
nullishamy <amycodes@null.net>
2022-04-15 19:40:52 +0100

    feat(types): refactor & add debug derives
    this refactors parts of the constant pool definitions and
    adds debug derivation for data dumping

--- src/structs/raw/constant_pool.rs
+++ src/structs/raw/constant_pool.rs
@@ -2,7 +2,7 @@ use anyhow::{anyhow, Result};
 use core::result::Result::{Err, Ok};
 use enum_as_inner::EnumAsInner;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub enum Tag {
     Utf8,
     Integer,
@@ -25,29 +25,29 @@ pub enum Tag {
 
 impl Tag {
     pub fn from_tag_byte(tag: u8) -> Result<Self> {
-        match tag {
-            1 => Ok(Tag::Utf8),
-            3 => Ok(Tag::Integer),
-            4 => Ok(Tag::Float),
-            5 => Ok(Tag::Long),
-            6 => Ok(Tag::Double),
-            7 => Ok(Tag::Class),
-            8 => Ok(Tag::String),
-            9 => Ok(Tag::FieldRef),
-            10 => Ok(Tag::MethodRef),
-            11 => Ok(Tag::InterfaceMethodRef),
-            12 => Ok(Tag::NameAndType),
-            15 => Ok(Tag::MethodHandle),
-            16 => Ok(Tag::MethodType),
-            17 => Ok(Tag::Dynamic),
-            18 => Ok(Tag::InvokeDynamic),
-            19 => Ok(Tag::Module),
-            20 => Ok(Tag::Package),
-            _ => Err(anyhow!("unknown constant pool tag {}", tag)),
-        }
+        Ok(match tag {
+            1 => Tag::Utf8,
+            3 => Tag::Integer,
+            4 => Tag::Float,
+            5 => Tag::Long,
+            6 => Tag::Double,
+            7 => Tag::Class,
+            8 => Tag::String,
+            9 => Tag::FieldRef,
+            10 => Tag::MethodRef,
+            11 => Tag::InterfaceMethodRef,
+            12 => Tag::NameAndType,
+            15 => Tag::MethodHandle,
+            16 => Tag::MethodType,
+            17 => Tag::Dynamic,
+            18 => Tag::InvokeDynamic,
+            19 => Tag::Module,
+            20 => Tag::Package,
+            _ => return Err(anyhow!("unknown constant pool tag {}", tag)),
+        })
     }
 
-    fn loadable(&self) -> bool {
+    pub fn loadable(&self) -> bool {
         matches!(
             self,
             Tag::Integer
@@ -63,104 +63,104 @@ impl Tag {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Utf8Data {
     pub length: u16,
     pub bytes: Vec<u8>,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct IntegerData {
     pub bytes: u32,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct FloatData {
     pub bytes: f32,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct LongData {
     //TODO: this has to take up 2 entries (?)
     pub low_bytes: u32,
     pub high_bytes: u32,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct DoubleData {
     //TODO: this has to take up 2 entries (?)
     pub low_bytes: f32,
     pub high_bytes: f32,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct ClassData {
     pub name_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct StringData {
     pub utf8_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct FieldRefData {
     pub class_index: u16,
     pub name_and_type_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct MethodRefData {
     pub class_index: u16,
     pub name_and_type_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct InterfaceMethodRefData {
     pub class_index: u16,
     pub name_and_type_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct NameAndTypeData {
     pub name_index: u16,
     pub descriptor_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct MethodHandleData {
     pub reference_kind: u8,
     pub reference_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct MethodTypeData {
     pub descriptor_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct DynamicData {
     pub bootstrap_method_attr_index: u16,
     pub name_and_type_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct InvokeDynamicData {
     pub bootstrap_method_attr_index: u16,
     pub name_and_type_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct ModuleData {
     pub name_index: u16,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct PackageData {
     pub name_index: u16,
 }
 
-#[derive(Clone, EnumAsInner)]
+#[derive(Clone, EnumAsInner, Debug)]
 pub enum Data {
     Utf8(Utf8Data),
     Integer(IntegerData),
@@ -181,7 +181,7 @@ pub enum Data {
     Package(PackageData),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct PoolEntry {
     pub tag: Tag,
     pub data: Data,
