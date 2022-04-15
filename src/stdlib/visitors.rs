ec5dc4b812be0ac6419cc88cbff69de47546fab6
nullishamy <amycodes@null.net>
2022-04-15 23:19:16 +0100

    feat(std): add stdout boilerplate
    this adds some template class data so that the runtime can load a value
    for stdout

--- src/stdlib/visitors.rs
+++ src/stdlib/visitors.rs
@@ -1,13 +1,52 @@
+use parking_lot::lock_api::RwLock;
+use std::ops::Deref;
 use std::sync::Arc;
 
+use crate::runtime::heap::object::JVMObject;
+use crate::structs::bitflag::ClassFileAccessFlags;
+use crate::structs::loaded::attribute::Attributes;
+use crate::structs::loaded::classfile::MetaData;
+use crate::structs::loaded::constant_pool::{ClassData, ConstantPool, Utf8Data};
+use crate::structs::loaded::constructors::Constructors;
+use crate::structs::loaded::field::Fields;
+use crate::structs::loaded::interface::Interfaces;
+use crate::structs::loaded::method::Methods;
 use crate::LoadedClassFile;
 
-pub fn visit_system(_class: Arc<LoadedClassFile>) {
-    //FIXME: add the system values here
+pub fn visit_system(class: Arc<LoadedClassFile>) {
+    let sysout = LoadedClassFile {
+        const_pool: ConstantPool {
+            entries: Default::default(),
+        },
+        meta: MetaData {
+            minor_version: 0,
+            major_version: 62,
+        },
+        access_flags: ClassFileAccessFlags::from_bits(000000).unwrap(),
+        this_class: Arc::new(ClassData {
+            name: Arc::new(Utf8Data {
+                str: "sysout".to_string(),
+            }),
+        }),
+        super_class: None,
+        interfaces: Interfaces { entries: vec![] },
+        fields: RwLock::new(Fields {
+            entries: vec![],
+            statics: Default::default(),
+        }),
+        methods: RwLock::new(Methods { entries: vec![] }),
+        constructors: Constructors { entries: vec![] },
+        attributes: Attributes { entries: vec![] },
+        package: None,
+    };
 
-    // class
-    //     .fields
-    //     .write()
-    //     .statics
-    //     .insert("out".to_string(), Rc::new_zeroed());
+    let sysout = JVMObject {
+        class: Arc::new(sysout),
+    };
+
+    class
+        .fields
+        .write()
+        .statics
+        .insert("out".to_string(), Arc::new(sysout));
 }
