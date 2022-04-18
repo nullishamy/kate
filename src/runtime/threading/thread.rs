61a722563e69c80cfe8a6044374630a935edb56f
nullishamy <amycodes@null.net>
2022-04-18 17:32:45 +0100

    feat(vm): add local variable support
    this adds local variable support to VMThread(s).

--- src/runtime/threading/thread.rs
+++ src/runtime/threading/thread.rs
@@ -1,12 +1,13 @@
 use parking_lot::Mutex;
 use tokio::sync::oneshot;
 
-use crate::runtime::stack::{OperandType, Stack};
+use crate::runtime::stack::{Stack, StackValue};
 use crate::runtime::threading::result::ThreadResult;
-use crate::structs::types::{PrimitiveWithValue, Type};
+use crate::structs::types::{PrimitiveWithValue, RefOrPrim};
 
 pub struct VMThread {
-    pub operand_stack: Mutex<Stack<OperandType>>,
+    pub operand_stack: Mutex<Stack<StackValue>>,
+    pub locals: Mutex<Vec<StackValue>>,
     pub call_stack: Mutex<Stack<CallStackEntry>>,
     pub pc: usize,
     pub name: String,
@@ -17,6 +18,7 @@ impl VMThread {
         Self {
             operand_stack: Mutex::new(Stack::new(0)),
             call_stack: Mutex::new(Stack::new(0)),
+            locals: Mutex::new(Vec::new()),
             pc: 0,
             name,
         }
@@ -26,10 +28,14 @@ impl VMThread {
     pub fn run(self) -> oneshot::Receiver<ThreadResult> {
         let (send, recv) = oneshot::channel::<ThreadResult>();
 
-        tokio::spawn(async move { send.send(Ok(Type::Primitive(PrimitiveWithValue::Int(1)))) });
+        //TODO: implement thread running
+        tokio::spawn(
+            async move { send.send(Ok(RefOrPrim::Primitive(PrimitiveWithValue::Int(1)))) },
+        );
 
         recv
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct CallStackEntry {}
