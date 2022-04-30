2f51cfc78c66a480275e4b5400dce69ea0774c88
nullishamy <amycodes@null.net>
2022-04-30 22:27:21 +0100

    feat(runtime): improve threading support
    this expands on the runtime threading support, namely by implementing parts of the run() function, altering the thread result to be consistent and implementing a thread manager to handle all running threads on the VM.

--- src/runtime/threading/thread.rs
+++ src/runtime/threading/thread.rs
@@ -1,41 +1,65 @@
-use parking_lot::Mutex;
+use parking_lot::{Mutex, RwLock};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use tokio::sync::oneshot;
 
+use crate::runtime::heap::object::JVMObject;
 use crate::runtime::stack::{Stack, StackValue};
 use crate::runtime::threading::result::ThreadResult;
+use crate::structs::loaded::method::MethodEntry;
 use crate::structs::types::{PrimitiveWithValue, RefOrPrim};
+use crate::{Args, CallSite, ClassLoader, LoadedClassFile, VM};
 
+#[derive(Debug)]
 pub struct VMThread {
-    pub operand_stack: Mutex<Stack<StackValue>>,
-    pub locals: Mutex<Vec<StackValue>>,
-    pub call_stack: Mutex<Stack<CallStackEntry>>,
-    pub pc: usize,
     pub name: String,
+    pub call_stack: Mutex<Stack<StackFrame>>,
+
+    method: Arc<MethodEntry>,
 }
 
 impl VMThread {
-    pub fn new(name: String) -> Self {
+    pub fn new(name: String, method: Arc<MethodEntry>) -> Self {
         Self {
-            operand_stack: Mutex::new(Stack::new(0)),
-            call_stack: Mutex::new(Stack::new(0)),
-            locals: Mutex::new(Vec::new()),
-            pc: 0,
             name,
+            call_stack: Mutex::new(Stack::new()),
+            method,
         }
     }
 
     // consume self, we can only run once
-    pub fn run(self) -> oneshot::Receiver<ThreadResult> {
+    pub fn run(self: Arc<Self>, vm: &VM, args: Args) -> oneshot::Receiver<ThreadResult> {
         let (send, recv) = oneshot::channel::<ThreadResult>();
 
+        let mut loader = vm.system_classloader.write();
+        let thread_class = loader.load_class("java/lang/Thread").unwrap();
+
+        let this = JVMObject {
+            class: Arc::clone(&thread_class),
+        };
+
         //TODO: implement thread running
-        tokio::spawn(
-            async move { send.send(Ok(RefOrPrim::Primitive(PrimitiveWithValue::Int(1)))) },
-        );
+        let callsite = CallSite::new(thread_class, self, self.method, this);
+        tokio::spawn(async move { send.send() });
 
         recv
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct CallStackEntry {}
+#[derive(Debug, Clone)]
+pub struct StackFrame {
+    pub operand_stack: Stack<StackValue>,
+    pub locals: Vec<StackValue>,
+    pub callsite: CallSite,
+}
+
+impl StackFrame {
+    pub fn new(callsite: CallSite) -> Self {
+        //FIXME: use attrs to determine these
+        Self {
+            operand_stack: Stack::new(),
+            locals: Vec::with_capacity(30),
+            callsite,
+        }
+    }
+}
