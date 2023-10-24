92d0a5dabd844b4a728a45e47c93e0fef5233d3b
nullishamy <git@amyerskine.me>
2023-10-24 21:14:01 +0100

    feat: long const tests

---
+++ tests/instruction/LongConsts.java
@@ -0,0 +1,16 @@
+// RUN: javac %s -d %t
+// RUN: kate --test --cp %t LongConsts | filecheck %s
+class LongConsts {
+  public static native void print(long l);
+
+  public static void main(String[] args) {
+    long zero = 0;
+    long one = 1;
+
+    // CHECK: 0
+    print(zero);
+
+    // CHECK: 1
+    print(one);
+  }
+}
