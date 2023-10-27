34e9f7ffc98cb88f458f11e3a9a7abc8c0cd7470
nullishamy <git@amyerskine.me>
2023-10-27 22:42:28 +0000

    Deploying to gh-pages from @ nullishamy/kate@8199fef6e356012dc813adc0703c0352af59ac39 🚀

--- interpreter_two/sidebar-items.js
+++ interpreter_two/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["class","field","instance_method","static_method"],"mod":["bytecode","native","object"],"struct":["Context","VM"]};
+window.SIDEBAR_ITEMS = {"macro":["arg","class","field","instance_method","pop","static_method"],"mod":["bytecode","native","object"],"struct":["Context","VM"]};
