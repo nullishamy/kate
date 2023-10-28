90e9d75f424794c8a6d1799d851986286ba5baa5
nullishamy <git@amyerskine.me>
2023-10-28 13:59:24 +0100

    feat: make tests run async

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import glob
 import re
-import subprocess
 import os
 import argparse
 import time
+import asyncio
 
 THIS_FILE = os.path.dirname(os.path.realpath(__file__))
 TEMP_DIR = os.path.join(THIS_FILE, '.temp')
@@ -63,20 +63,25 @@ def perform_subs(run_cmd, subs, *args):
 
   return tmp
 
-def run_cmd(cmd, display_command):
+async def run_cmd(cmd, display_command):
   if display_command:
     print(f'{Colours.BLUE}run:{Colours.END}', cmd)
-  return subprocess.run(cmd, capture_output=True, shell=True)
 
-def display_failure(exec):
-  print(f"""{Colours.RED}{Colours.UNDERLINE}fail:{Colours.END}
+  return await asyncio.create_subprocess_shell(
+    cmd,
+    stdout=asyncio.subprocess.PIPE,
+    stderr=asyncio.subprocess.PIPE
+  )
+
+async def display_failure(exec, context):
+  print(f"""{Colours.RED}{Colours.UNDERLINE}fail: {context}{Colours.END}
 {Colours.RED}stdout:{Colours.END}
-{exec.stdout.decode('utf-8')}
+{(await exec.stdout.read()).decode('utf-8')}
 {Colours.RED}stderr:{Colours.END}
-{exec.stderr.decode('utf-8')}
+{(await exec.stderr.read()).decode('utf-8')}
   """)
 
-def run_tests(sources, args):
+async def run_tests(sources, args):
   passes = []
   fails = []
 
@@ -84,8 +89,7 @@ def run_tests(sources, args):
     print(f'{Colours.WARNING}warn:{Colours.END} no sources found')
     return passes, fails
 
-  for source_location in sources:
-    print(f'{Colours.PEACH}test:{Colours.END}', source_location)
+  async def run_for_source(source_location):
     with open(source_location) as source:
       content = source.read()
       run_cmds = map(
@@ -93,49 +97,66 @@ def run_tests(sources, args):
         extract_runners(content)
       )
 
-      for run in run_cmds:
-        did_fail = False
+      procs = []
+      exits = []
 
-        exec = run_cmd(run, args.verbose)
-        if exec.returncode != 0:
-          display_failure(exec)
-          did_fail = True
-      
-      if did_fail:
-          fails.append(source_location)
-      else:
-          print(f'{Colours.GREEN}ok:{Colours.END}', source_location)
-          passes.append(source_location)
+      async for proc in (await run_cmd(run, False) for run in run_cmds):
+        exit = await proc.wait()
+        procs.append(proc)
+        exits.append(exit)
+
+      return source_location, list(zip(procs, exits))
+  
+  results = await asyncio.gather(*[
+    run_for_source(source) for source in sources
+  ])
+
+  for source, execution_set in results:
+    did_any_fail = False
+    for proc, exit in execution_set:
+      if exit != 0:
+        did_any_fail = True
+        print(await display_failure(proc, source))
+
+    if did_any_fail:
+        fails.append(source)
+    else:
+        print(f'{Colours.GREEN}ok:{Colours.END}', source)
+        passes.append(source)
 
   return passes, fails
 
 
-def main():
+async def main():
   args = make_parser().parse_args()
   print(f'{Colours.GREEN}building...{Colours.END}')
-  build = run_cmd("cargo build", args.verbose)
-  if build.returncode != 0:
-    display_failure(build)
+  build = await run_cmd("cargo build", args.verbose)
+  build = await build.wait()
+  if build != 0:
+    await display_failure(build, "build")
     return
 
   print(f'{Colours.GREEN}cleaning...{Colours.END}')
-  clean = run_cmd(f'rm -rf {TEMP_DIR}', args.verbose)
-  if clean.returncode != 0:
-    display_failure(clean)
+  clean = await run_cmd(f'rm -rf {TEMP_DIR}', args.verbose)
+  clean = await clean.wait()
+  if clean != 0:
+    await display_failure(clean, "clean")
     return
   
   print(f'{Colours.GREEN}pre-run steps ok{Colours.END}')
   print()
+  print(f'{Colours.GREEN}running...{Colours.END}')
+  print()
 
   start = time.time()
-  passes, fails = run_tests(filter_sources(get_sources(['java']), args.filter), args)
+  passes, fails = await run_tests(filter_sources(get_sources(['java']), args.filter), args)
   end = time.time()
   duration = end - start
 
   print()
   print()
-  print()
   print(f'{Colours.PEACH}testing concluded{Colours.END} ({round(duration, 3)}s)')
   print(f'{Colours.GREEN}pass:{Colours.END} {len(passes)} - {Colours.RED}fail:{Colours.END} {len(fails)}')
 
-main()
+if __name__ == '__main__':
+  asyncio.run(main())
