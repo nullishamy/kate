481a9d1c7faed719ae3f261a2aaadcad0c197aaa
nullishamy <git@amyerskine.me>
2023-12-08 17:45:13 +0000

    test: add read file test

--- tests/program.rs
+++ tests/program.rs
@@ -1,9 +1,13 @@
 mod util;
 
-use crate::util::{
-    builder::using_main,
-    compare, execute, expected, inline, state, TestResult,
+use std::{fs::File, io::Write, path::PathBuf};
+
+use util::{
+    builder::{direct, using_helpers},
+    TMP_DIR,
 };
+
+use crate::util::{builder::using_main, compare, execute, expected, inline, state, TestResult};
 
 #[test]
 pub fn hello_world() -> TestResult {
@@ -122,6 +126,62 @@ pub fn internal_error() -> TestResult {
         .with_output("\\----------------------------------------------------------/")
         .with_output("Uncaught exception in main: testing, internal errors")
         .with_output("  at InternalError.main");
+
+    compare(got, expected);
+
+    Ok(())
+}
+
+#[test]
+pub fn read_file() -> TestResult {
+    let state = state().init().init_std();
+
+    let tmp_dir: PathBuf = TMP_DIR.into();
+    let tmp_path = tmp_dir.join("basic-read.txt");
+    let mut tmp_file = File::create(tmp_path.clone())?;
+    tmp_file.write_all("test test test".as_bytes())?;
+
+    let content = format!(
+        r#"
+        import java.io.File;
+        import java.io.FileInputStream;
+
+        class ReadFile {{
+            public static void main(String[] args) throws Exception {{
+                File file = new File("{}");
+                FileInputStream inputStream = new FileInputStream(file);
+                byte[] bytes = inputStream.readAllBytes();
+                System.out.print("Content: ");
+                System.out.println(bytes);
+            }}
+        }}
+        "#,
+        tmp_path.display()
+    );
+
+    let source = direct("ReadFile", &content);
+
+    let got = execute(state, inline(source)?)?;
+    let expected = expected().has_success();
+
+    compare(got, expected);
+
+    Ok(())
+}
+#[test]
+pub fn new_thread_set_priority() -> TestResult {
+    let state = state().init();
+
+    let source = using_main(
+        "NewThreadSetPriority",
+        r#"
+            Thread t = new Thread();
+            t.setPriority(1);
+        "#,
+    );
+
+    let got = execute(state, inline(source)?)?;
+    let expected = expected().has_success();
 
     compare(got, expected);
 
